d73cfd6377a756972e181053b3ae535cc1753a28
Marco Regueiras (uie25542) <marco.regueiras@conti-engineering.com>
2021-09-09 17:00:47 +0100

    DRYed up the code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,19 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def show      
     end
+
     def index 
         @articles = Article.all
     end 
+
     def new
         @article = Article.new
     end
+
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         #render plain: @article.inspect
         if @article.save
             flash[:notice] = "Article was created successfully."
@@ -18,21 +22,30 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
     def edit
-        @article = Article.find(params[:id])
     end
+
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article #or redirect_to article_path(@article)
         else
             render 'edit'
         end
     end
+    
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
+    end
+    
+    private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
